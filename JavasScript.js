42ad0ec3e66b28aa912a0bcd5063bb19b2b00612
Ahmad-A92 <ahmad.alsyed92@gmail.com>
2021-03-18 14:36:29 +0200

    added new features at my page using JS

---
+++ JavasScript.js
@@ -0,0 +1,16 @@
+// alert as greeting massages for the users 
+alert ("Hello How are You, We hope you will enjoy looking at the information covered on the site");
+
+// To notify the user if he enter the incorrect page at the web site 
+yourOcuupation = prompt("What is your occupation");
+if (yourOcuupation == "Civil Engineering") {document.write("<h3>"+"Yes you are on the right page"+"</h3>");
+} else {document.write("<h3>"+"you are at the civil engineering page, please check the links at the top and sellect the one sutiabe to your occupation"+"</h3>")};
+
+// counsile  order 
+console.log("please be aware to submitted all of your tasks and assignments before the sunday 21/3/2021 ");
+
+// To check the age of the users 
+
+var yourAge = confirm("Please confirm that you are above 18");
+if (yourAge==true) {document.write("<p3>"+"you have confiremd that you are above 18"+"</p3>");
+} else {document.write("<p3>"+"you have confiremd that you are above 18"+"</p3>")};
