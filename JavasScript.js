19b772d0aafba58452044b134f289fb21bd34bbf
Ahmad-A92 <ahmad.alsyed92@gmail.com>
2021-03-22 16:46:50 +0200

    Lab06

--- JavasScript.js
+++ JavasScript.js
@@ -2,16 +2,24 @@
 alert ("Hello How are You, We hope you will enjoy looking at the information covered on the site");
 
 // To notify the user if he enter the incorrect page at the web site & using while statement to check the inputs. 
+
 yourOcuupation = prompt("What is your occupation");
+
 while (yourOcuupation !== "Civil Engineering" && yourOcuupation !=="Structural Engineering"){
     yourOcuupation = prompt("please select Civil Engineering/Structural Engineering");
 }
 
-if (yourOcuupation == "Civil Engineering") {
-    document.write("<h3>"+"Hellow Civil Enginner"+"</h3>");
-}else if (yourOcuupation == "Structural Engineering") { 
-    document.write("<h3>"+"Hellow Structural Enginner"+"</h3>");
-} 
+function occupationMsssages(occup) {
+
+    if (yourOcuupation == "Civil Engineering") {
+        document.write("<h3>"+"Hellow Civil Enginner"+"</h3>");
+    }else if (yourOcuupation == "Structural Engineering") { 
+        document.write("<h3>"+"Hellow Structural Enginner"+"</h3>");
+    } 
+}
+
+// occupationMsssages(yourOcuupation)
+
 
 // counsile  order 
 console.log("This web page is just for training within software development courses");
@@ -19,12 +27,19 @@ console.log("This web page is just for training within software development cour
 // To check the age of the users 
 
 var yourAge = confirm("Please confirm that you are above 18");
-if (yourAge==true) {document.write("<p3>"+"you have confiremd that you are above 18"+"</p3>");
+function AgeConf(ageX){
+    if (yourAge==true) {document.write("<p3>"+"you have confiremd that you are above 18"+"</p3>");
 } else {document.write("<p3>"+"you have confiremd that you are above 18"+"</p3>");
 }
+}
+// AgeConf(yourAge)
 // for loop statement 
 RepeatedPictures= prompt("Enter the number of time you want to see engineering logo repeated at the below side");
 
-for (var i=0; i<RepeatedPictures; i++){
-    document.write('<img src="download.jpg" name="logo" width="435" height="500" />'+(i+1));
-}  
+function Logo(NumberTimes){
+    for (var i=0; i<RepeatedPictures; i++){
+        document.write('<img src="download.jpg" name="logo" width="435" height="500" />'+(i+1));
+    }  
+    
+} 
+// log(RepeatedPictures)
